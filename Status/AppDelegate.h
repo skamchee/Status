bc21b57e1c432fc70be1218f3493cc6527b3115b
Spencer Kamchee <s.kamchee@gmail.com>
2016-03-09 21:16:22 -0600

    Initial Commit

---
+++ Status/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Status
+//
+//  Created by Spencer Kamchee on 3/9/16.
+//  Copyright © 2016 Spencer Kamchee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Status/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Status
+//
+//  Created by Spencer Kamchee on 3/9/16.
+//  Copyright © 2016 Spencer Kamchee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
